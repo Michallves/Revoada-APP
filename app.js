06d1e9571343901c251e6564522279c6daf2e31e
michallves <maicon159951@outlook.com>
2022-08-08 19:39:31 -0300

    projeto

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ export default () => {
   return (
       <NavigationContainer>
         <StatusBar
-          style={'light'}
+          style={'dark'}
           translucent={true}
           backgroundColor={'transt'}
         />
